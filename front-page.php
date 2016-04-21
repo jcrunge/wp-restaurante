111c418cf39ad2d57cac7425fa5590b409d94f5e
salomonmac <geforcesalomonti@gmail.com>
2016-04-20 21:51:25 -0500

    update salomon

--- front-page.php
+++ front-page.php
@@ -11,7 +11,7 @@ the_post();
     	            <div class=" col-lg-4 clearfix">
     	                <div class="thumbnail borde">
 	                 <h3 class="text-center">Platillo del Mes</h3>
-            <img src="img/Platillo.jpg" alt="Platillo" class="img-responsive text-center">
+            <img src="<?php echo get_template_directory_uri();?>/img/Platillo.jpg" alt="Platillo" class="img-responsive text-center">
           <div class="caption">
             <p class="text-justify">Cada mes sacamos un nuevo platillo donde la gente puede degustar este platillo.</p>
             <bottom class="btn btn-primary">Saber más</bottom>
@@ -23,7 +23,7 @@ the_post();
                    <div class=" col-lg-4 clearfix">
                <div class="thumbnail borde">
 	             <h3 class="text-center">Reservaciones</h3>
-                  <img src="img/Instalaciones.jpg" alt="Instalaciones" class="img-responsive">
+                  <img src="<?php echo get_template_directory_uri();?>/img/Instalaciones.jpg" alt="Instalaciones" class="img-responsive">
       <div class="caption">
             <p class="text-justify">Ahora es más fácil reserva solo llena y formulario y listo .</p>
         <bottom class="btn btn-primary">Saber más</bottom>
@@ -35,7 +35,7 @@ the_post();
                <div class=" col-lg-4 clearfix">
                <div class="thumbnail borde">
 	             <h3 class="text-center">Personal</h3>
-      <img src="img/Personal.jpg" alt="Personal" class="img-responsive">
+      <img src="<?php echo get_template_directory_uri();?>/img/Personal.jpg" alt="Personal" class="img-responsive">
       <div class="caption">
         <p class="text-justify">Nuestro personal está certificado para darte la mejor experiencia para los eventos o fiestas.</p>
         <bottom class="btn btn-primary">Saber más</bottom>
